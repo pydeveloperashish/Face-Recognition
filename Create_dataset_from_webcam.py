6806932a144fca50dbc1b5c8cb7d2e2869494e68
Sek Davata <sagardspeed2@gmail.com>
2020-01-02 16:07:40 +0530

    Update Create_dataset_from_webcam.py
    Here i added a way, so now no need to create folder manually for storing photos by webcam, this script automatically create a folder and store images in that per user

--- Create_dataset_from_webcam.py
+++ Create_dataset_from_webcam.py
@@ -1,6 +1,18 @@
 import cv2
 import sys
+import os
+from os import path
+
 cpt = 0
+users = 0;
+
+while True:
+	user_images_path = "Give path here/train-images/"+str(users) #Give path to  train-images/ and keep image%04i.jpg as it is in this line. Your images will be stored at train-images/ respective index number folder
+	if(path.exists(user_images_path)):
+		users += 1
+	else:
+		os.mkdir(user_images_path)
+		break
 
 vidStream = cv2.VideoCapture(0)
 while True:
@@ -9,11 +21,9 @@ while True:
     
     cv2.imshow("test window", frame) # show image in window
     
-    cv2.imwrite("Give path here/train-images/0/image%04i.jpg" %cpt, frame)    #Give path to  train-images/0/ and keep image%04i.jpg as it is in this line. Your images will be stored at train-images/0/ folder
+    cv2.imwrite(user_images_path+"image%04i.jpg" %cpt, frame)
     cpt += 1
-    
-        
-
+   
     if cv2.waitKey(10)==ord('q'):
         break
         
